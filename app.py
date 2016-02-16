3053c402a37e4d6c2d817517761d733cb74cccff
SowmyaBawgi <bawgisowmya@gmail.com>
2016-02-16 22:26:56 +0545

    Update app.py

--- app.py
+++ app.py
@@ -29,16 +29,15 @@ def upload():
     uploaded_files = request.files.getlist("file[]")
     filenames = []
     for file in uploaded_files:
-        # Check if the file is one of the allowed extensions
+        # Check if the file is one of the allowed types/extensions
         if file and allowed_file(file.filename):
-            
+          
             filename = (file.filename)
-           
+            # Move the file form the temporal folder to the upload folder
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # Save the filename into a list
+            # Save the filename into a list, we'll use it later
             filenames.append(filename)
            
-    # Load an html page with a link to each uploaded file
     return render_template('upload.html', filenames=filenames)
 
 
@@ -47,5 +46,25 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
+
+
+@app.route('/remove', methods=['POST'])
+def remove(filename):
+    filenames=[]
+
+    if(os.path.isfile(filename)):
+        os.remove(app.config['UPLOAD_FOLDER'],filename)
+
+    path = "/uploads"
+    dirs = os.listdir(path)
+
+# This would print all the files and directories
+    for file in dirs:
+        print(file)
+        filenames.append(filename)
+
+    return render_template("uploads.html",filenames=filenames)
+
+
 if __name__ == '__main__':
-    app.run(  debug=True   )
+    app.run(debug=True)
